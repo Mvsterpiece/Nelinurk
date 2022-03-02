248757a22ce202909775a89e6d4174e5b3c9a6e4
opilane <opilane@HP-CZC6297BB1>
2022-03-02 14:15:13 +0200

    Add project files.

---
+++ Nelinurk.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nelinurk
+{
+	class Nelinurk
+	{
+		// Поля
+		double A;
+		double B;
+		int nurk;
+		string tuup;
+		
+		// Конструкторы
+		public Nelinurk() { }
+		public Nelinurk(int korgus, int laius)
+		{
+			A = korgus;
+			B = laius;
+		}
+
+		//Свойства
+		public string Tuup 
+		{
+			get
+			{
+				if(A == B && nurk==90)
+				{
+					tuup = "Ruut";
+				}
+				else if (A==B && nurk!=90)
+				{
+					tuup = "Romb";
+				}
+				else if (A!=B && nurk==90)
+				{
+					tuup = "Ristkülik";
+				}
+
+				return tuup;
+			}
+		}
+
+
+	}
+}
